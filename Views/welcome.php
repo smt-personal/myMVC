9520a6ef40d680b4932cd5acf0cfc7bad2933ea4
developer <developer@site.net>
2018-11-11 12:04:02 -0800

    renamed welcom.view to welcome

---
+++ Views/welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>BB MVC</title>
+
+		<style>
+			body{
+				background: chartreuse;
+				margin: 0;
+			}
+			.container{
+				width: 1100px;
+				max-width: 90%;
+				margin: 100px auto 0;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<h1>Welcome to BB MVC</h1>
+		</div>
+	</body>
+</html>
