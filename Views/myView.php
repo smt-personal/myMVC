cb8224d3538d8e99ae370cb6395a6b04589d273e
developer <developer@site.net>
2018-11-10 19:33:03 -0800

    initial commit

---
+++ Views/myView.php
@@ -0,0 +1,15 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>My Page</title>
+
+		<style>
+			body{
+				background: chartreuse;
+			}
+		</style>
+	</head>
+	<body>
+		<p>This is my webpage</p>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$reqArr = explode("/", $_SERVER['REQUEST_URI']);
+array_shift($reqArr);
+array_shift($reqArr);
+if(count($reqArr) > 0){
+	@include('Controllers/'.$reqArr[0].'.php');	
+
+	$test = new $reqArr[0]();
+
+	if(count($reqArr) > 1){
+		$func = $reqArr[1]; 
+		$test->$func();
+		$test->loadView("myView");
+	}
+}
+
+
+
+
+//use BaseController
+
+//print_r($_SERVER);
+
+
+//$test = new BaseController();
+
